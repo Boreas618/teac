d3f374443026551f46fea48cb58958e919e71ed9
luBai <20307130208@fudan.edu.cn>
2023-11-07 09:24:01 +0800

    assignmen 4

--- Assignment-4/Solution/compiler.cpp
+++ Assignment-4/Solution/compiler.cpp
@@ -31,18 +31,7 @@ int main(int argc, char * argv[]) {
 
     line = 1;
     col = 1;
-    
-    freopen(argv[1], "r", stdin);  
-    ofstream ASTStream;
-    // ASTStream.open(argv[2]);
 
-    yyparse();
-    
-    aroot = aA_Program(root);
-    // print_aA_Program(aroot, ASTStream);
-    // ASTStream.close();
-
-    // check_Prog(&std::cout, aroot);
     string input_name = argv[1];
     auto dot_pos = input_name.find('.');
     if(dot_pos == input_name.npos)
@@ -51,6 +40,18 @@ int main(int argc, char * argv[]) {
         return -1;
     }
     string file_name(input_name.substr(0,dot_pos));
+    
+    freopen(argv[1], "r", stdin);  
+    ofstream ASTStream;
+    ASTStream.open(file_name+".ast");
+
+    yyparse();
+    
+    aroot = aA_Program(root);
+    print_aA_Program(aroot, ASTStream);
+    ASTStream.close();
+
+    // check_Prog(&std::cout, aroot);
 
     ofstream LLVMStream;
     LLVMStream.open(file_name + ".ll");
