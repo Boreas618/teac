45d1d90dbbe92a15eb94e0bdc63a585aca3009e1
aisr <china.xuhui@gmail.com>
2025-02-08 18:54:56 +0800

    refactor

--- Compiler/loopunroll.hpp
+++
@@ -1,3 +0,0 @@
-#pragma once
-int loopunroll(A_stmt stm, Temp_label_front brelabel, Temp_label_front conlabel,
-               Temp_label_front name);
--- Compiler/templabel.cpp
+++
@@ -1,6 +0,0 @@
-#include "singleton.hpp"
-#include "templabel.hpp"
-#include <string>
-using std::string;
-using std::string;
-string Temp_newlabel_front() { return Singleton::instance().LabelManager.newlabel(); }
